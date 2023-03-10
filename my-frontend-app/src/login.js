4e56175ea6f80da02ca7c519f5df14d3e6b92b9a
tugi <blueqwinn@gmail.com>
2023-03-10 10:56:45 +0300

    added my login .js

---
+++ my-frontend-app/src/login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import UserLogin from "./User.login";
+import { useState, errorMessages,  } from "react";
+
+const [errorMessages,  SetErrorMessages] = useState({});
+const [isSubmitted,  setSubmitted] = useState(false);
+
+// To generate code for error message
+const renderErrorMessage= (name) =>
+name === errorMessages.name && (
+    <div className="error">{errorMessages.message}</div>
+)
+
+// Code For login form
+const renderForm = (
+    <div className="form">
+<form>
+<div  className="input-container">
+<label>Username </label>
+<input type="text" name="uname" required />
+{renderErrorMessage("uname")}
+</div>
+<div className="input-container">
+<label>Password</label>
+{renderErrorMessage("pass")}
+</div>
+<div className="button-container"> 
+<input type="submit" />
+</div>
+</form>
+ </div>
+)
+
+
+const handleSubmit= (event) =>{
+    //Prevent page reload
+    event.preventDefault();
+};
+
+<form onSubmit={handleSubmit}></form>
